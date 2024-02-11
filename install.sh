36429b985807411f99d4fcbb54c1b765bd31c8c6
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-02-11 22:15:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 python -m venv doccli_src/.venv
 doccli_src/.venv/bin/pip install requests
 
-sudo mv doccli /usr/local/bin
-sudo mv doccli_src/* /usr/local/bin/doccli_src/
+sudo mv doccli/doccli /usr/local/bin
+sudo mv doccli/doccli_src/* /usr/local/bin/doccli_src/
 
 sudo rm -rf doccli
