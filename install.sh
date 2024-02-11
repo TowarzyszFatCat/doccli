02f0b897224dd4a21e9faeae6c5f02d181a8426b
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-02-11 22:20:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ doccli_src/.venv/bin/pip install requests
 
 sudo mv doccli/doccli /usr/local/bin
 sudo mkdir /usr/local/bin/doccli_src
-sudo mv doccli/doccli_src/* /usr/local/bin/doccli_src/
+sudo mv doccli/doccli_src/ /usr/local/bin/doccli_src/
 
 sudo rm -rf doccli
