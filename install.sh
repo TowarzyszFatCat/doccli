896d6ee4698ef4f2de0870ff9b90515cb6c380a6
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-02-13 00:45:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 sudo mv doccli/doccli /usr/local/bin
 sudo mv doccli/doccli_src /usr/local/bin/
 python -m venv /usr/local/bin/doccli_src/venv
-/usr/local/bin/doccli_src/venv/bin/pip install requests discord-rich-presence
+/usr/local/bin/doccli_src/venv/bin/pip install requests pypresence
 
 sudo rm -rf doccli
