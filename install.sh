12ff5b70ea281623d9f9f548395c72db894fecfa
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-02-13 12:36:51 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo mv doccli/doccli /usr/local/bin
-sudo mv doccli/doccli_src /usr/local/bin/
-python -m venv /usr/local/bin/doccli_src/venv
-/usr/local/bin/doccli_src/venv/bin/pip install requests pypresence
-
-sudo rm -rf doccli
