d15a174637f9744765ab149c4d2b95c718465629
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-02-16 00:36:44 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-sudo chmod +x $SCRIPT_DIR/doccli
-sudo cp $SCRIPT_DIR/doccli /usr/local/bin
-
-sudo mkdir /usr/local/bin/doccli_src
-sudo cp $SCRIPT_DIR/* /usr/local/bin/doccli_src
-
-cd /usr/local/bin/doccli_src && sudo python -m venv .venv
-cd /usr/local/bin/doccli_src && sudo .venv/bin/pip install requests pypresence
-
-# Remove install files
-sudo rm -rf $SCRIPT_DIR
+if [ "${PWD##*/}" == "doccli" ] ; then
+  sudo chmod +x doccli
+  sudo cp doccli /usr/local/bin
+  
+  sudo mkdir /usr/local/bin/doccli_src
+  sudo cp * /usr/local/bin/doccli_src
+  
+  cd /usr/local/bin/doccli_src && sudo python -m venv .venv
+  cd /usr/local/bin/doccli_src && sudo .venv/bin/pip install requests pypresence
+else
+  echo "WEJDZ DO FOLDERU DOCCLI!"
+fi
