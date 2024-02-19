73c02568c0664686d5a4c5b8b258cb969439877a
TowarzyszFatCat <kloczdupy228@gmail.com>
2024-02-19 21:14:51 +0100

    fixed linux location

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 if [ "${PWD##*/}" == "doccli" ] ; then
   sudo chmod +x doccli
-  sudo cp doccli /usr/local/bin
+  sudo mv doccli /usr/local/bin
   
-  sudo mkdir /usr/local/bin/doccli_src
-  sudo cp * /usr/local/bin/doccli_src
+  sudo mkdir ~/.doccli_src
+  sudo mv * ~/.doccli_src
   
-  cd /usr/local/bin/doccli_src && sudo python -m venv .venv
-  cd /usr/local/bin/doccli_src && sudo .venv/bin/pip install requests pypresence
+  cd ~/.doccli_src && sudo python -m venv .venv
+  cd ~/.doccli_src && sudo .venv/bin/pip install requests pypresence
 else
   echo "WEJDZ DO FOLDERU DOCCLI!"
 fi
