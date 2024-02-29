4c37f8bb29807174e1db98d286ce0990764fe3e1
tfc <kloczdupy228@gmail.com>
2024-02-29 19:27:07 +0100

    fixed paths

--- modules/global_variables_module.py
+++ modules/global_variables_module.py
@@ -3,7 +3,7 @@ from os import path, name as system_name, environ
 VERSION: str = "v2.1"
 config: dict = {}
 
-ROOT_DIR = environ['USERPROFILE']
+ROOT_DIR = environ['USERPROFILE'] if system_name == "nt" else environ['HOME']
 
 CONFIG_PATH = path.join(ROOT_DIR, "doccli.config")
 LOG_PATH = path.join(ROOT_DIR, "doccli.log")
