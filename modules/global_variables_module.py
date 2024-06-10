37a4faf0979e8c6abd43b8a667bda5f2eb054e12
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-06-10 16:04:25 +0200

    Delete modules directory

--- modules/global_variables_module.py
+++
@@ -1,9 +0,0 @@
-from os import path, name as system_name, environ
-
-VERSION: str = "v2.2"
-config: dict = {}
-
-ROOT_DIR = environ['USERPROFILE'] if system_name == "nt" else environ['HOME']
-
-CONFIG_PATH = path.join(ROOT_DIR, "doccli.config")
-LOG_PATH = path.join(ROOT_DIR, "doccli.log")
