f878974fa674f67ab242f2f4e85254ab73b507bd
tfc <kloczdupy228@gmail.com>
2024-06-10 16:37:38 +0200

    final version of 2.5 update

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if [ "${PWD##*/}" == "~" ] ; then
-  sudo chmod +x doccli/doccli
-  sudo mv doccli/doccli /usr/local/bin
+sudo chmod +x doccli/doccli
+sudo mv doccli/doccli /usr/local/bin
 
-  sudo mv doccli ~/.doccli_src
+sudo mv doccli ~/.doccli_src
 
-  sudo chmod 777 ~/.doccli_src
-  sudo chmod 777 ~/.doccli_src/*
-  
-  cd ~/.doccli_src && sudo python3 -m venv .venv
-  cd ~/.doccli_src && sudo .venv/bin/pip install requests yt-dlp inquirerpy
-else
-  echo "WEJDZ DO FOLDERU DOCCLI!"
-fi
+sudo chmod 777 ~/.doccli_src
+sudo chmod 777 ~/.doccli_src/*
+
+cd ~/.doccli_src && sudo python3 -m venv .venv
+cd ~/.doccli_src && sudo .venv/bin/pip install requests yt-dlp inquirerpy
