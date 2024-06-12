c4c17c6f63168137e166396f7b7cef6ed86e59c0
TowarzyszFatCat <68988781+TowarzyszFatCat@users.noreply.github.com>
2024-06-12 23:28:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ sudo chmod 777 ~/.doccli_src
 sudo chmod 777 ~/.doccli_src/*
 
 cd ~/.doccli_src && sudo python3 -m venv .venv
-cd ~/.doccli_src && sudo .venv/bin/pip install requests yt-dlp inquirerpy termcolor
+cd ~/.doccli_src && sudo .venv/bin/pip install requests yt-dlp inquirerpy termcolor pypresence
